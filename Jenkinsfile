f87d581c2e8a055bb20e2ed7d6af67e2929df0ff
Alex Earl <slide.o.mix@gmail.com>
2020-03-04 01:58:32 -0700

    Initial Windows packer image build (#8)
    Co-authored-by: Tim Jacomb <t.jacomb@kainos.com>

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,52 +1,65 @@
 if (env.CHANGE_ID) {
-    properties([
-        buildDiscarder(logRotator(numToKeepStr: '10')),
-    ])
-}
-else {
-    properties([
-        buildDiscarder(logRotator(numToKeepStr: '96')),
-        pipelineTriggers([[$class:"SCMTrigger", scmpoll_spec:"H/10 * * * *"]]),
-    ])
+  properties([
+    buildDiscarder(logRotator(numToKeepStr: '10')),
+  ])
+} else {
+  properties([
+    buildDiscarder(logRotator(numToKeepStr: '96')),
+    pipelineTriggers([[$class: "SCMTrigger", scmpoll_spec: "H/10 * * * *"]]),
+  ])
 }
 
 pipeline {
-  agent {
-    docker {
-      args '--entrypoint=""'
-      image 'hashicorp/packer:1.5.4'
-      label 'docker&&linux'
-    }
-  }
+  agent none
 
   stages {
-    stage('Validate') {
-      steps {
-        sh 'packer validate --var-file validate-vars.json ubuntu-18-agent.json'
-      }
-    }
+    stage('ValidateAndBuild') {
+      matrix {
+        agent {
+          docker {
+            args '--entrypoint=""'
+            image 'hashicorp/packer:1.5.4'
+            label 'docker&&linux'
+          }
+        }
+        axes {
+          axis {
+            name 'AGENT'
+            values 'ubuntu-18', 'windows-2019'
+          }
+        }
+        stages {
+          stage('Validate') {
+            steps {
+              sh 'packer validate --var-file validate-vars.json ${AGENT}-agent.json'
+              sh 'packer validate --var-file validate-vars.json ${AGENT}-agent.json'
+            }
+          }
 
-    stage('Build') {
-      environment {
-        AZURE_SUBSCRIPTION_ID = credentials('packer-azure-subscription-id')
-        AZURE_CLIENT_ID = credentials('packer-azure-client-id')
-        AZURE_CLIENT_SECRET = credentials('packer-azure-client-secret')
-      }
+          stage('Build') {
+            environment {
+              AZURE_SUBSCRIPTION_ID = credentials('packer-azure-subscription-id')
+              AZURE_CLIENT_ID = credentials('packer-azure-client-id')
+              AZURE_CLIENT_SECRET = credentials('packer-azure-client-secret')
+            }
 
-      when {
-        branch 'master'
-      }
+            when {
+              branch 'master'
+            }
 
-      steps {
-        sh """
-          packer build \
-            --var location="East US 2" \
-            --var resource_group_name="prod-packer-images" \
-            --var subscription_id="$AZURE_SUBSCRIPTION_ID" \
-            --var client_id="$AZURE_CLIENT_ID" \
-            --var client_secret="$AZURE_CLIENT_SECRET" \
-            ubuntu-18-agent.json
-        """
+            steps {
+              sh """
+                packer build \
+                  --var location="East US 2" \
+                  --var resource_group_name="prod-packer-images" \
+                  --var subscription_id="$AZURE_SUBSCRIPTION_ID" \
+                  --var client_id="$AZURE_CLIENT_ID" \
+                  --var client_secret="$AZURE_CLIENT_SECRET" \
+                  ${AGENT}-agent.json
+              """
+            }
+          }
+        }
       }
     }
   }
---
+++ windows-2019-provision.ps1
@@ -0,0 +1,81 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Add-Type -AssemblyName System.Web
+
+$baseDir = 'c:\azurecsdir'
+
+$JDKUrl = 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-{0}/OpenJDK11U-jdk_x64_windows_hotspot_{1}.zip' -f [System.Web.HTTPUtility]::UrlEncode($env:JAVA_VERSION),$env:JAVA_VERSION.Replace('+', '_')
+$destinationJDKZipPath = "$baseDir\adoptOpenJDK.zip"
+$javaHome = '{0}\jdk-{1}' -f $baseDir,$env:JAVA_VERSION
+
+$GITUrl = 'https://github.com/git-for-windows/git/releases/download/v{0}.windows.1/MinGit-{0}-64-bit.zip' -f $env:GIT_VERSION
+$destinationGitZipPath = "$baseDir\MinGit.zip"
+$GITPath = "$baseDir\git\cmd\"
+
+$MavenUrl = 'https://apache.osuosl.org/maven/maven-3/{0}/binaries/apache-maven-{0}-bin.zip' -f $env:MAVEN_VERSION
+$destinationMavenZipPath = "$baseDir\maven.zip"
+$mavenPath = '{0}\apache-maven-{1}\bin' -f $baseDir,$env:MAVEN_VERSION
+
+function DownloadFile($url, $targetFile)
+{
+   $uri = New-Object "System.Uri" "$url"
+   $request = [System.Net.HttpWebRequest]::Create($uri)
+   $request.set_Timeout(15000) #15 second timeout
+   $response = $request.GetResponse()
+   $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
+   $responseStream = $response.GetResponseStream()
+   $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
+   $buffer = new-object byte[] 10KB
+   $count = $responseStream.Read($buffer,0,$buffer.length)
+   $downloadedBytes = $count
+   while ($count -gt 0) {
+       $targetStream.Write($buffer, 0, $count)
+       $count = $responseStream.Read($buffer,0,$buffer.length)
+       $downloadedBytes = $downloadedBytes + $count
+       Write-Progress -activity "Downloading file '$($url.split('/') | Select-Object -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
+   }
+   Write-Progress -activity "Finished downloading file '$($url.split('/') | Select-Object -Last 1)'"
+   $targetStream.Flush()
+   $targetStream.Close()
+   $targetStream.Dispose()
+   $responseStream.Dispose()
+}
+
+# Checking if this is first time script is getting executed, if yes then downloading JDK, Git and VS Tools
+If(-not((Test-Path $destinationJDKZipPath)))
+{
+    New-Item -ItemType Directory -Path $baseDir -Force
+    DownloadFile $JDKUrl $destinationJDKZipPath
+    Expand-Archive -Path $destinationJDKZipPath -DestinationPath $baseDir
+    Remove-Item -Force $destinationJDKZipPath
+
+    DownloadFile $GITUrl $destinationGitZipPath
+    New-Item -ItemType Directory -Path "$baseDir\git" -Force
+    Expand-Archive -Path $destinationGitZipPath -DestinationPath $baseDir\git
+    & "$GITPath\git" config --system core.autocrlf false
+    & "$GITPath\git" config --system core.longpaths true
+    Remove-Item -Force $destinationGitZipPath
+
+    DownloadFile $MavenUrl $destinationMavenZipPath
+    Expand-Archive -Path $destinationMavenZipPath -DestinationPath $baseDir
+    Remove-Item -Force $destinationMavenZipPath
+
+    # update the system path to include Git, Java and Maven
+    $oldPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+    $newPath = "$oldPath;$javaHome\bin;$GITPath;$MavenPath"
+    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+    # setup JAVA_HOME environment variable
+    New-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name JAVA_HOME -Value $javaHome
+}
+
+& $env:SystemRoot\\System32\\Sysprep\\Sysprep.exe /oobe /generalize /quiet /quit
+while($true) {
+    $imageState = Get-ItemProperty HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Setup\\State | Select-Object ImageState
+    if($imageState.ImageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') {
+        Write-Output $imageState.ImageState
+        Start-Sleep -s 10
+    } else {
+        break
+    }
+}
