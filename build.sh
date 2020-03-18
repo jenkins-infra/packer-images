4bc1696ecb07bb2e6182514cbdebbe973c5e9919
Olblak <me@olblak.com>
2020-03-18 15:44:26 +0100

    Move build logic from Jenkinsfile to bash

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+: "${CLOUD:?Cloud not defined [aws,azure]}"
+: "${AGENT:?Agent not defined}"
+: "${ARCHITECTURE:?Architecture not defined[amd64,arm64]}"
+: "${LOCATION:?Location not defined}"
+
+
+function isTemplateExist() {
+  if [ ! -f "${CLOUD}/${AGENT}-agent.${ARCHITECTURE}.json" ]; then
+    echo "Error file not found: ${CLOUD}/${AGENT}-agent.${ARCHITECTURE}.json"
+    exit 1
+  fi
+}
+
+function buildAzure(){
+  : "${RESOURCE_GROUP_NAME:?Resource Group Name Defined}"
+  packer build \
+      --force \
+      --var location="$LOCATION" \
+      --var resource_group_name="$RESOURCE_GROUP_NAME" \
+      --var subscription_id="$AZURE_SUBSCRIPTION_ID" \
+      --var client_id="$AZURE_CLIENT_ID" \
+      --var client_secret="$AZURE_CLIENT_SECRET" \
+      "./${CLOUD}/${AGENT}-agent.${ARCHITECTURE}.json"
+}
+
+function buildAWS(){
+  packer build \
+      --force \
+      --var location="$LOCATION" \
+      --var aws_access_key="$AWS_ACCESS_KEY_ID" \
+      --var aws_secret_key="$AWS_SECRET_ACCESS_KEY" \
+      "./${CLOUD}/${AGENT}-agent.${ARCHITECTURE}.json"
+}
+
+function build() {
+  isTemplateExist
+  case $CLOUD in
+    aws)
+      buildAWS
+      ;;
+    azure)
+      buildAzure
+      ;; 
+  esac
+}
+
+
+build
