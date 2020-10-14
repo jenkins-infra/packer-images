c530e546d008ff905f6058502ae6fd7275ca31f4
Gareth Evans <gareth@bryncynfelin.co.uk>
2020-10-14 10:56:18 +0100

    chore: attempt to simplify the download of binaries

--- scripts/windows-2019-provision.ps1
+++ scripts/windows-2019-provision.ps1
@@ -71,27 +71,8 @@ $downloads = [ordered]@{
 }
 
 function DownloadFile($url, $targetFile) {
-   $uri = New-Object "System.Uri" "$url"
-   $request = [System.Net.HttpWebRequest]::Create($uri)
-   $request.set_Timeout(15000) #15 second timeout
-   $response = $request.GetResponse()
-   $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
-   $responseStream = $response.GetResponseStream()
-   $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
-   $buffer = new-object byte[] 10KB
-   $count = $responseStream.Read($buffer,0,$buffer.length)
-   $downloadedBytes = $count
-   while ($count -gt 0) {
-       $targetStream.Write($buffer, 0, $count)
-       $count = $responseStream.Read($buffer,0,$buffer.length)
-       $downloadedBytes = $downloadedBytes + $count
-       Write-Progress -activity "Downloading file '$($url.split('/') | Select-Object -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
-   }
-   Write-Progress -activity "Finished downloading file '$($url.split('/') | Select-Object -Last 1)'"
-   $targetStream.Flush()
-   $targetStream.Close()
-   $targetStream.Dispose()
-   $responseStream.Dispose()
+    Write-Host "Downloading $url"
+    Invoke-WebRequest $url -OutFile $targetFile
 }
 
 foreach($k in $downloads.Keys) {
