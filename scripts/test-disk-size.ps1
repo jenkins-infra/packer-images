6454e24670d8eaab767a5ee86f5c685dffcd4651
Gareth Evans <gareth@bryncynfelin.co.uk>
2020-11-10 10:00:49 +0000

    chore: improved error on disk space warning

--- scripts/test-disk-size.ps1
+++ scripts/test-disk-size.ps1
@@ -4,14 +4,14 @@ $ErrorActionPreference = 'Stop'
 $drives = @("C");
  
 # The minimum disk size to check for raising the warning
-$minSize = 100GB;
+$minSize = 99GB;
 
 foreach ($d in $drives) {
     Write-Host ("Checking drive " + $d + " ...");
     $disk = Get-PSDrive $d;
     $total = $disk.Free + $disk.Used
     if ($total -lt $minSize) {
-        Write-Error ("Drive " + $d + " has less than " + $minSize + " total size");
+        Write-Error ("Drive " + $d + " has less than " + $minSize + " total size=" + $total);
         exit 1
     }
 }
