2ec8849996a1df4b959d02bfba9a98da796a5cce
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-07-23 15:10:04 +0100

    Update scripts/ubuntu-20-provision.sh

--- scripts/ubuntu-20-provision.sh
+++ scripts/ubuntu-20-provision.sh
@@ -68,7 +68,6 @@ apt-get install -y --no-install-recommends \
   qemu-user-static
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker run --rm -t arm64v8/ubuntu uname -m # test that qemu is working
 
 ## Install git
 if [ -n "${GIT_VERSION}" ]
