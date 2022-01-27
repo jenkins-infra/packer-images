36d6dc4f5b42244703ef3a42f0f40a08f72f8fa9
smerle33 <smerle@cloudbees.com>
2022-01-27 17:38:58 +0100

    using no-tty and the local gpg key for docker

--- scripts/ubuntu-20-provision.sh
+++ scripts/ubuntu-20-provision.sh
@@ -68,7 +68,8 @@ function install_docker() {
     gnupg-agent \
     software-properties-common
 
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  #using the local version of the docker public key avoid curl errors
+  gpg --no-tty --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg docker.pgp
   echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
