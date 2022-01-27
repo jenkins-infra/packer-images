5e2e84a3873807873b07fe2ff605f1cb82ceafe0
smerle33 <smerle@cloudbees.com>
2022-01-27 14:44:01 +0100

    add no tty for gpg

--- scripts/ubuntu-20-provision.sh
+++ scripts/ubuntu-20-provision.sh
@@ -68,7 +68,7 @@ function install_docker() {
     gnupg-agent \
     software-properties-common
 
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --no-tty --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
