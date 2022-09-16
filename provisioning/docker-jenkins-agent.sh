428eb137a788bc68884e64096af539bdd4eaa8dd
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2022-09-16 10:34:01 +0200

    fix(ubuntu docker) set locale en_US.UTF-8 as default (#341)
    * fix (locale): set the locale
    
    * try to add the locale en_US.UTF-8
    
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- provisioning/docker-jenkins-agent.sh
+++ provisioning/docker-jenkins-agent.sh
@@ -6,7 +6,13 @@ export DEBIAN_FRONTEND=noninteractive # Avoid APT or dpkg asking questions
 
 # Docker Ubuntu image are missing required tools: let's install it as a preliminary
 apt-get update --quiet
-apt-get install --yes --no-install-recommends ca-certificates curl sudo software-properties-common
+apt-get install --yes --no-install-recommends ca-certificates curl sudo software-properties-common locales
+
+# force the en_US.UTF-8 locale
+echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+locale-gen en_US.UTF-8
 
 echo "= Retrieve agent.jar"
 curl --create-dirs --fail --silent --show-error --location --output /usr/share/jenkins/agent.jar "https://repo.jenkins-ci.org/public/org/jenkins-ci/main/remoting/${JENKINS_REMOTING_VERSION}/remoting-${JENKINS_REMOTING_VERSION}.jar"
