99adae374231589e86a19143de12b38b2dc23464
smerle33 <stephane.merle+jenkins@gmail.com>
2023-12-18 11:31:15 +0100

    feat(doctl): add doctl install on linux only

--- provisioning/ubuntu-provision.sh
+++ provisioning/ubuntu-provision.sh
@@ -348,6 +348,14 @@ function install_docker_compose(){
   chmod a+x /usr/local/bin/docker-compose
 }
 
+## Ensure that DOCTL is installed
+function install_doctl(){
+  curl --fail --silent --location --show-error --output /usr/local/bin/doctl \
+    "https://github.com/digitalocean/doctl/releases/download/v${DOCTL_VERSION}/doctl-${DOCTL_VERSION}-linux-$(uname -m).tar.gz" | tar -xzv doctl
+  chmod a+x /usr/local/bin/doctl
+}
+
+
 ## Ensure that maven is installed and configured (version from environment)
 function install_maven() {
   curl --fail --silent --location --show-error --output "/tmp/apache-maven-${MAVEN_VERSION}-bin.tar.gz" \
@@ -633,6 +641,7 @@ function main() {
   install_JA_requirements
   install_qemu
   install_azcopy
+  install_doctl
   install_python
   install_docker_compose
   install_maven
