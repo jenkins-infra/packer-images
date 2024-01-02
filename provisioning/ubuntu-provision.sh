247fbf0fc4ec888ecf488eb8df1148c36d61e6ec
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2024-01-02 08:31:49 +0100

    Apply suggestions from code review
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- provisioning/ubuntu-provision.sh
+++ provisioning/ubuntu-provision.sh
@@ -355,19 +355,19 @@ function install_doctl(){
     "https://github.com/digitalocean/doctl/releases/download/v${DOCTL_VERSION}/doctl-${DOCTL_VERSION}-linux-${ARCHITECTURE}.tar.gz" | tar --extract --gunzip --directory="${install_dir}"/ doctl
 }
 
-## Ensure that HELM and it's plugins are installed
+## Ensure that`helm` and its plugins are installed
 function install_helm(){
   install_dir=/usr/local/bin
   curl --fail --silent --location --show-error \
     "https://get.helm.sh/helm-v${HELM_VERSION}-linux-${ARCHITECTURE}.tar.gz" | \
     tar --extract --gunzip --strip-components 1 --directory="${install_dir}"/ "linux-${ARCHITECTURE}/helm"
 
-  helm plugin install https://github.com/databus23/helm-diff --version v${HELM_DIFF_VERSION}
-  helm plugin install https://github.com/jkroepke/helm-secrets --version v${HELM_SECRETS_VERSION}
-  helm plugin install https://github.com/aslafy-z/helm-git.git --version v${HELM_GIT_VERSION}
+  su - "${username}" -c "helm plugin install https://github.com/databus23/helm-diff --version v${HELM_DIFF_VERSION}"
+  su - "${username}" -c "helm plugin install https://github.com/jkroepke/helm-secrets --version v${HELM_SECRETS_VERSION}"
+  su - "${username}" -c "helm plugin install https://github.com/aslafy-z/helm-git.git --version v${HELM_GIT_VERSION}"
 }
 
-## Ensure that HELMFILE is installed
+## Ensure that`helmfile` is installed
 function install_helmfile(){
   install_dir=/usr/local/bin
   curl --fail --silent --location --show-error \
@@ -375,7 +375,7 @@ function install_helmfile(){
     tar --extract --gunzip --directory="${install_dir}"/  helmfile
 }
 
-## Ensure that SOPS is installed
+## Ensure that`sops` is installed
 function install_sops(){
   install_dir=/usr/local/bin
   curl --fail --silent --location --show-error --output "${install_dir}"/sops \
