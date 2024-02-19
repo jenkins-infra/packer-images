b27efd02fa7678ba8c76d6ded1894fea5d350aa3
smerle33 <stephane.merle+jenkins@gmail.com>
2024-02-19 18:01:49 +0100

    add gpg-agent

--- updatecli/scripts/fetch-git-apt-latest-version.sh
+++ updatecli/scripts/fetch-git-apt-latest-version.sh
@@ -16,7 +16,7 @@ done
   apt-get update -q
   apt-get install --yes --no-install-recommends \
     software-properties-common \
-    gpg
+    gpg-agent
   add-apt-repository -y ppa:git-core/ppa
   apt-get update -q
 } 1>&2 # Only write logs to stderr to avoid polluting updatecli's source (retrieved from the stdout)
