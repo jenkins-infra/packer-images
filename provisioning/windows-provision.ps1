8f82a6446a3fcd099cf7f003b2b70900ccfa2b95
Alex Earl <slide.o.mix@gmail.com>
2024-12-09 06:20:17 -0700

    Add install of .NET 3.5
    Adds install of .NET 3.5 for building the MSI installer

--- provisioning/windows-provision.ps1
+++ provisioning/windows-provision.ps1
@@ -259,6 +259,8 @@ $downloads = [ordered]@{
             # Installation of make for Windows
             & "choco.exe" install make --yes --no-progress --limit-output --fail-on-error-output;
             & "choco.exe" install vagrant --yes --no-progress --limit-output --fail-on-error-output --version "${env:VAGRANT_VERSION}";
+            # install .NET 3.5 for MSI build
+            & "choco.exe" install dotnet3.5 --yes --no-progress --limit-output --fail-on-error-output
             # Append a ".1" as all ruby packages in chocolatey have this suffix. Not sure why (maybe a package build id)
             & "choco.exe" install ruby --yes --no-progress --limit-output --fail-on-error-output --version "${env:RUBY_VERSION}.1";
             & "choco.exe" install packer --yes --no-progress --limit-output --fail-on-error-output --version "${env:PACKER_VERSION}";
