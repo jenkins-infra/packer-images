e9fa9c9eed9eaa771ff7e30ea11396806be6c8cf
smerle33 <stephane.merle+jenkins@gmail.com>
2025-02-28 12:00:29 +0100

    new call to SysprepInstance and revert windows-update plugin

--- provisioning/EC2Launch-install.ps1
+++ provisioning/EC2Launch-install.ps1
@@ -1,3 +1,4 @@
+echo --------------------------------------------- EC2Launch_install started
 mkdir $env:USERPROFILE\Desktop\EC2Launch
 $Url = "https://s3.amazonaws.com/ec2-downloads-windows/EC2Launch/latest/EC2-Windows-Launch.zip"
 $DownloadZipFile = "$env:USERPROFILE\Desktop\EC2Launch\" + $(Split-Path -Path $Url -Leaf)
@@ -6,3 +7,9 @@ $Url = "https://s3.amazonaws.com/ec2-downloads-windows/EC2Launch/latest/install.
 $DownloadZipFile = "$env:USERPROFILE\Desktop\EC2Launch\" + $(Split-Path -Path $Url -Leaf)
 Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
 & $env:USERPROFILE\Desktop\EC2Launch\install.ps1
+
+Import-Module C:\ProgramData\Amazon\EC2-Windows\Launch\Module\Ec2Launch.psm1
+Import-LocalizedData -BaseDirectory C:\ProgramData\Amazon\EC2-Windows\Launch\Module\ -FileName 'Ec2Launch.psd1' -BindingVariable moduleManifest
+echo ModuleVersion
+$moduleManifest.Get_Item('ModuleVersion')
+echo --------------------------------------------- EC2Launch_install finished
