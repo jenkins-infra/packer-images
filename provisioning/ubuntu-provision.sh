a88363174e7d3e4a34a957c9c1338dc3ce0dc0b3
Damien Duportal <damien.duportal@gmail.com>
2026-05-13 14:09:51 +0200

    Update ubuntu-provision.sh

--- provisioning/ubuntu-provision.sh
+++ provisioning/ubuntu-provision.sh
@@ -590,18 +590,25 @@ function install_nodejs() {
 }
 
 function install_playwright() {
-  ### Install playwright's dependencies before the module itself
-  ## The command 'npx playwright install-deps --dry-run' prints the expectd command for installing dependencies.
-  # But this command requires `sudo` access (which the ${username} user does not have).
-  # Also, the `root` user does not have access to the ASDF setup.
-  # Don't forget to change dir and to remove any `stderr` to avoid polluting the evakuated command
-  playwright_deps_install_command="$(su - "${username}" -c "\
-    source ${asdf_install_dir}/asdf.sh \
-    && npx playwright install-deps --dry-run")"
-  eval "${playwright_deps_install_command}"
-
   # Install pinned playwright globally
   su - "${username}" -c "source ${asdf_install_dir}/asdf.sh && npm install -g playwright@${PLAYWRIGHT_VERSION}"
+
+  # Install required system dependencies - https://playwright.dev/docs/browsers#install-system-dependencies
+  # Note: need to extract the list of missing packages as jenkins user but install as root
+  playwright_system_deps="$(su - "${username}" -c "\
+    source ${asdf_install_dir}/asdf.sh \
+    && npx playwright install-deps --dry-run \
+      | grep -v 'Missing system dependencies' `# Remove initial header line` \
+    || true `# playwright install-deps command fails if packages are missing. let's report later for diagnosing` " \
+  )"
+
+  # Report in build logs
+  echo "Installing the following system dependencies for playwright:"
+  echo "${playwright_system_deps}"
+  echo
+
+  # Perform the installation as root user
+  echo "${playwright_system_deps}" | xargs apt-get install -y
 }
 
 ## Install Launchable with python3 in its own virtual environment
